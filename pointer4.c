6e144c70367b7a661273099cafc05260b6d62f00
Kusuma2006 <kusumadeeksha671@gmail.com>
2024-12-10 12:23:00 +0530

    Add files via upload

---
+++ pointer4.c
@@ -0,0 +1,19 @@
+//array and pointers
+#include<stdio.h>
+
+int main()
+{
+	int size=0,a[100],i,*large=a;
+	scanf("%d",&size);
+	for( i=0;i<size;scanf("%d",a+i++));
+
+    
+    for(i=1;i<size;i++)
+    {
+        if(*large <a[i])
+        large=&a[i];
+    }
+    printf("largest element is %d",*large);
+    
+
+}
